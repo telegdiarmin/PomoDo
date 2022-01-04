a4a3dc5790d4da5b40790d3567275ca01838d609
telegdiarmin <armin.telegdi@gmail.com>
2022-01-04 11:40:57 +0100

    Fixed core functionality to start counter at button click

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
+const timer = {
+  isRunning: false,
+  startTime: 0,
+//   currentMinute: minutes,
+//   currentSecond: seconds,
+}
+
 const counterFrame = document.querySelector("#js-counter");
 const mainButton = document.querySelector("#js-main-button");
 
+//mainButton szövegének változtatása
 function setMainButtonText() {
-  mainButton.textContent = "Start";
+  if (timer.isRunning = "false") {
+    mainButton.textContent = "Start";
+  } else {
+    mainButton.textContent = "Stop";
+  }
 }
 
+//mainButton szövegének változtatása
 setMainButtonText();
 
 function timerFunction() {
-  const startingMinutes = 25;
+  let minutes = "";
+  let seconds = "";
+  const startingMinutes = 1;
   let time = startingMinutes * 60;
   setInterval(timer, 1000);
   function timer() {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
+    minutes = Math.floor(time / 60);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = time % 60;
+    seconds = seconds < 10 ? "0" + seconds : seconds; //ternáris operátor!
     counterFrame.innerHTML = `${minutes}:${seconds}`;
-    time--;
+    if (time >= 1) {
+      time--;
+    }
   }
 }
 
